2f81c57412a6dfdc3d4a35e75d027728be91119a
bgbahoue <bgbahoue@gmail.com>
2017-05-16 13:51:54 +1100

    util > Created `ContainsKey`trait and added implementations for serde types

--- src/util.rs
+++ src/util.rs
@@ -1,62 +1,63 @@
 // =======================================================================
 // LIBRARY IMPORTS
 // =======================================================================
+#[cfg(feature = "amiwo_serde")]
 use serde_json;
 
 use std::borrow::Borrow;
 use std::hash::Hash;
-use std::iter::IntoIterator;
 
 // =======================================================================
 // TRAIT DECLARATION
 // =======================================================================
-pub trait AmiwoUtil {
+/// Adds a `contains_keys()` method to any Map like object
+pub trait ContainsKeys<K> {
+    fn contains_keys<Q: ?Sized>(&self, keys: &[&Q]) -> bool 
+        where
+            K: Borrow<Q>,
+            Q: Hash + Eq + Ord;
 }
-
-/*
-pub trait ContainKeys {
-    fn contains_keys<K>(&self, keys: &[K]) -> bool 
-        where K: Into<String> + Hash + Ord + Eq;
-}
-*/
 
 // =======================================================================
-// FUNCTIONS DECLARATION
+// TRAIT IMPLEMENTATION
 // =======================================================================
-// Note: You can create a function that accepts both &[String] and &[&str] using the AsRef trait: see http://stackoverflow.com/questions/41179659/convert-vector-of-string-into-slice-of-str-in-rust
-pub fn contains_keys<K>(value: &serde_json::Value, keys: &[K]) -> bool 
-    where K: AsRef<str>
-{
-    value.as_object().map_or(
-        false, 
-        |obj| keys.iter().all(|ref key| obj.contains_key(key.as_ref()))
-    )
-}
-
-/*
-impl ContainKeys for serde_json::Value {
-    fn contains_keys<K>(&self, keys: &[K]) -> bool 
-    where
-        K: Into<String> + Hash + Ord + Eq,
+#[cfg(feature = "amiwo_serde")]
+impl ContainsKeys<String> for serde_json::Value {
+    fn contains_keys<Q: ?Sized>(&self, keys: &[&Q]) -> bool 
+        where
+            String: Borrow<Q>,
+            Q: Hash + Eq + Ord
     {
-    self.as_object().map_or(
-        false, 
-        |obj| keys.iter().all(|ref key| obj.contains_key(key))
-    )
+        self.as_object().map_or(
+            false, 
+            |obj| keys.iter().all(|ref key| obj.contains_key(key))
+        )
     }
 }
-*/
+
+#[cfg(feature = "amiwo_serde")]
+impl ContainsKeys<String> for serde_json::map::Map<String, serde_json::Value> {
+    fn contains_keys<Q: ?Sized>(&self, keys: &[&Q]) -> bool 
+        where
+            String: Borrow<Q>,
+            Q: Hash + Eq + Ord
+    {
+        keys.iter().all(|ref key| self.contains_key(key))
+    }
+}
+
 // =======================================================================
 // UNIT TESTS
 // =======================================================================
 #[cfg(test)]
 mod tests {
     use serde_json;
+    use super::ContainsKeys;
 
     #[test]
-    fn has_properties() {
+    fn contains_keys() {
         let obj: serde_json::Value = serde_json::from_str(r#"{"a": 1, "b": 2}"#).unwrap();
-        assert_eq!(super::contains_keys(&obj, &["a", "b"]), true); 
-        assert_eq!(super::contains_keys(&obj, &["a", "b", "c"]), false); 
+        assert_eq!(obj.contains_keys(&["a", "b"]), true); 
+        assert_eq!(obj.contains_keys(&["a", "b", "c"]), false);
     }
 }
